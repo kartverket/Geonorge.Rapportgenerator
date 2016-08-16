3077e83a9c3b10f4c6bc33d97f55862f1a7b26fa
Benjamin Dehli <benjamin@arkitektum.no>
2016-08-16 13:34:28 +0200

    Add and include Kartverket Felleskomponenter

--- Kartverket.ReportGenerator/App_Start/BundleConfig.cs
+++ Kartverket.ReportGenerator/App_Start/BundleConfig.cs
@@ -8,6 +8,17 @@ namespace Kartverket.ReportGenerator
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new StyleBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/css/styles").Include(
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendor.min.css",
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendorfonts.min.css",
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/scripts").Include(
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/vendor.min.js",
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js"
+           ));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
---
+++ Kartverket.ReportGenerator/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace Kartverket.ReportGenerator.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string ApplicationVersionNumber(this HtmlHelper helper)
+        {
+            string versionNumber = WebConfigurationManager.AppSettings["BuildVersionNumber"];
+            return versionNumber;
+        }
+        public static string GeonorgeUrl(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["GeonorgeUrl"];
+        }
+        public static string GeonorgeArtiklerUrl(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["GeonorgeArtiklerUrl"];
+        }
+        public static string NorgeskartUrl(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["NorgeskartUrl"];
+        }
+        public static string SecureNorgeskartUrl(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["SecureNorgeskartUrl"];
+        }
+        public static string RegistryUrl(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["RegistryUrl"];
+        }
+        public static string ObjektkatalogUrl(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["ObjektkatalogUrl"];
+        }
+        public static bool DownloadServiceEnabled(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["DownloadServiceEnabled"] == "false" ? false : true;
+        }
+        public static string WebmasterEmail(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["WebmasterEmail"];
+        }
+        public static string EnvironmentName(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["EnvironmentName"];
+        }
+
+        public static string KartkatalogenUrl(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["KartkatalogenUrl"];
+        }
+    }
+}
