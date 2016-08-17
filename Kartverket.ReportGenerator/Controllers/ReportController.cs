f96c6117194c56f2c18a9bb5b2d54c4872bab55a
Dag Olav <dagolav@arkitektum.no>
2016-08-17 10:51:30 +0200

    Initial report view

---
+++ Kartverket.ReportGenerator/Controllers/ReportController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kartverket.ReportGenerator.Models;
+
+namespace Kartverket.ReportGenerator.Controllers
+{
+    public class ReportController : Controller
+    {
+        private ReportDbContext db = new ReportDbContext();
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            var reportQueries = db.ReportQueries.Include(r => r.MetadataEntry);
+            return View(reportQueries.ToList());
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
