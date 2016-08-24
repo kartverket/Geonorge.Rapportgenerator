0440e11daecc5cc751a02053bfe53316298e32ed
Dag Olav <dagolav@arkitektum.no>
2016-08-24 08:33:42 +0200

    Added list of ReportResultData to ReportResult

--- Kartverket.ReportApi/ReportResult.cs
+++ Kartverket.ReportApi/ReportResult.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace Kartverket.ReportApi
 {
     public class ReportResult
     {
         public int TotalDataCount;
+
+        public List<ReportResultData> Data;
         
     }
 }
