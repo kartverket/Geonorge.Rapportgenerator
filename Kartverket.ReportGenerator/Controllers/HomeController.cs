0442726426936aaf51184dec44c33fc229ffa712
Veronika Andersen <veronika@arkitektum.no>
2017-11-13 10:30:37 +0100

    Translate breadcrumb and error page and add english footer

--- Kartverket.ReportGenerator/Controllers/HomeController.cs
+++ Kartverket.ReportGenerator/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Kartverket.ReportGenerator.Helpers;
 
 namespace Kartverket.ReportGenerator.Controllers
 {
@@ -14,5 +15,29 @@ namespace Kartverket.ReportGenerator.Controllers
 
             return View();
         }
+
+        [Route("setculture/{culture}")]
+        public ActionResult SetCulture(string culture, string returnUrl)
+        {
+            // Validate input
+            culture = CultureHelper.GetImplementedCulture(culture);
+            // Save culture in a cookie
+            HttpCookie cookie = Request.Cookies["_culture"];
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = culture;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+
+            if (!string.IsNullOrEmpty(returnUrl))
+                return Redirect(returnUrl);
+            else
+                return RedirectToAction("Index");
+        }
     }
 }
+
--- Kartverket.ReportGenerator/Global.asax.cs
+++ Kartverket.ReportGenerator/Global.asax.cs
@@ -1,12 +1,15 @@
 ﻿using Autofac;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Kartverket.ReportGenerator.Models.Translations;
 
 namespace Kartverket.ReportGenerator
 {
@@ -22,5 +25,23 @@ namespace Kartverket.ReportGenerator
             DependencyConfig.Configure(new ContainerBuilder());
             log4net.Config.XmlConfigurator.Configure();
         }
+
+        protected void Application_BeginRequest()
+        {
+            var cookie = Context.Request.Cookies["_culture"];
+            if (cookie == null)
+            {
+                cookie = new HttpCookie("_culture", Culture.NorwegianCode);
+                HttpContext.Current.Response.Cookies.Add(cookie);
+            }
+
+            if (!string.IsNullOrEmpty(cookie.Value))
+            {
+                var culture = new CultureInfo(cookie.Value);
+                Thread.CurrentThread.CurrentCulture = culture;
+                Thread.CurrentThread.CurrentUICulture = culture;
+            }
+        }
     }
 }
+
