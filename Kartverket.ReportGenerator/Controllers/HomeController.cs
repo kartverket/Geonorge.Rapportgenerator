5bc062df7f745654de60e6e0dbff07eed417ea6e
Veronika Andersen <veronika@arkitektum.no>
2017-11-13 10:49:47 +0100

    Try bug fix language button

--- Kartverket.ReportGenerator/Controllers/HomeController.cs
+++ Kartverket.ReportGenerator/Controllers/HomeController.cs
@@ -15,29 +15,6 @@ namespace Kartverket.ReportGenerator.Controllers
 
             return View();
         }
-
-        [Route("setculture/{culture}")]
-        public ActionResult SetCulture(string culture, string returnUrl)
-        {
-            // Validate input
-            culture = CultureHelper.GetImplementedCulture(culture);
-            // Save culture in a cookie
-            HttpCookie cookie = Request.Cookies["_culture"];
-            if (cookie != null)
-                cookie.Value = culture;   // update cookie value
-            else
-            {
-                cookie = new HttpCookie("_culture");
-                cookie.Value = culture;
-                cookie.Expires = DateTime.Now.AddYears(1);
-            }
-            Response.Cookies.Add(cookie);
-
-            if (!string.IsNullOrEmpty(returnUrl))
-                return Redirect(returnUrl);
-            else
-                return RedirectToAction("Index");
-        }
     }
 }
 
