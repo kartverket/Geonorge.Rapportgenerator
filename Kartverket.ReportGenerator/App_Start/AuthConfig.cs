095f5bf6c8d74836d1646983ca82ecaf298af79f
Dag Olav Dahle <dagolav@arkitektum.no>
2018-11-26 10:55:33 +0100

    Api add new statistical report

---
+++ Kartverket.ReportGenerator/App_Start/AuthConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kartverket.ReportGenerator.App_Start
+{
+    public static class AuthConfig
+    {
+        public const string DatasetProviderRole = "dataset-provider";
+    }
+}
---
+++ Kartverket.ReportGenerator/PostDeploy.ps1
@@ -0,0 +1,2 @@
+﻿Copy-Item authentication.default.config authentication.config
+Copy-Item settings.default.config settings.config
